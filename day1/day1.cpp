f1d4081c332f38cc7685c0a6e61799ae87975283
Aslan Bennington-Barr <aslan.bennington.barr@gmail.com>
2023-12-15 12:09:02 +1300

    D1P2 finished

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -2,26 +2,39 @@
 #include <fstream>
 #include <algorithm>
 #include <string>
+#include <map>
 
 using namespace std;
 
-int part1(ifstream *data);
+string NUMBERS[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+int part1(ifstream &data);
+int part2(ifstream &data);
+string makeNumeric(string line);
 int getFirstNumber(string line);
 
 int main()
 {
     ifstream Testdata("test-data");
-    int calibration = part1(&Testdata);
-    cout << "Test calibration was: " << calibration << ". Expected: " << 142 << endl;
     ifstream Data("data");
-    calibration = part1(&Data);
+    int calibration;
+    calibration = part1(Testdata);
+    cout << "Test calibration was: " << calibration << ". Expected: " << 142 << endl;
+    calibration = part1(Data);
     cout << "Calibration was: " << calibration << ". Expected: " << 53194 << endl;
+
+    calibration = part2(Testdata);
+    cout << "P2 Test calibration was: " << calibration << endl;
+    calibration = part2(Data);
+    cout << "P2 Calibration was: " << calibration << endl;
 }
-int part1(ifstream *data)
+int part1(ifstream &data)
 {
+    data.clear();
+    data.seekg(0);
     string line;
     int dataSum = 0;
-    while (getline(*data, line))
+    while (getline(data, line))
     {
         int lineValue = 10 * getFirstNumber(line);
         reverse(line.begin(), line.end());
@@ -29,6 +42,47 @@ int part1(ifstream *data)
         dataSum += lineValue;
     }
     return dataSum;
+}
+
+int part2(ifstream &data)
+{
+    data.clear();
+    data.seekg(0);
+    string line;
+    int dataSum = 0;
+    while (getline(data, line))
+    {
+        line = makeNumeric(line);
+        int lineValue = 10 * getFirstNumber(line);
+        reverse(line.begin(), line.end());
+        lineValue += getFirstNumber(line);
+        dataSum += lineValue;
+    }
+    return dataSum;
+}
+
+string makeNumeric(string line)
+{
+    string output = "";
+    for (int i = 0; i < line.length(); i++)
+    {
+        int characterValue = (int)line[i] - 48;
+        if (characterValue >= 0 && characterValue <= 9)
+        {
+            output += to_string(characterValue);
+        }
+        for (int j = 0; j < sizeof(NUMBERS) / sizeof(NUMBERS[0]); j++)
+        {
+            string number = NUMBERS[j];
+            string number_substr = line.substr(i, number.length());
+
+            if (number_substr.compare(number) == 0)
+            {
+                output += to_string(j);
+            }
+        }
+    }
+    return output;
 }
 
 int getFirstNumber(string line)
