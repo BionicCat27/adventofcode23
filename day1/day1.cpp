1d3c465fc9b2639ef6a1d7d16e286c5f31035801
Aslan Bennington-Barr <aslan.bennington.barr@gmail.com>
2023-12-15 11:21:40 +1300

    Untidy correct D1P1 solution

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,39 +1,61 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
+int part1(ifstream *data);
+
 int main()
 {
-    ifstream TestData("data");
-    int expectedCalibrationValue = 142;
+    ifstream Testdata("test-data");
+    int calibration = part1(&Testdata);
+    cout << "Test calibration was: " << calibration << ". Expected: " << 142 << endl;
+    ifstream Data("data");
+    calibration = part1(&Data);
+    cout << "Calibration was: " << calibration << ". Expected: " << 53194 << endl;
+}
+int part1(ifstream *data)
+{
     string line;
     int dataSum = 0;
-    while (getline(TestData, line))
+    while (getline(*data, line))
     {
-        cout << line << endl;
         int lineValue = 0;
+        bool found = false;
         for (string::iterator iter = line.begin(); iter != line.end(); ++iter)
         {
             int characterValue = (int)*iter - 48;
             if (characterValue >= 0 && characterValue <= 9)
             {
                 lineValue += (characterValue * 10);
+                found = true;
                 break;
             }
         }
-        for (string::iterator iter = line.end(); iter != line.begin(); --iter)
+        if (!found)
+        {
+            cout << "Not found start: " << line << endl;
+        }
+        found = false;
+        reverse(line.begin(), line.end());
+        for (string::iterator iter = line.begin(); iter != line.end(); ++iter)
         {
             int characterValue = (int)*iter - 48;
             if (characterValue >= 0 && characterValue <= 9)
             {
                 lineValue += characterValue;
+                found = true;
                 break;
             }
         }
-        cout << "Found number: " << lineValue << endl;
+        if (!found)
+        {
+            cout << "Not found ends: " << line << endl;
+        }
+        // cout << "Found number: " << lineValue << endl;
         dataSum += lineValue;
     }
-    cout << "Count was " << dataSum << endl;
-    return 0;
+    return dataSum;
 }
